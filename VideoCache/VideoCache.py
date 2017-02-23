954cdb5087d8879e01e007bbb4afb300d3de202b
dylanmorroll <dylanjmmorroll@gmail.com>
2017-02-23 19:18:54 +0000

    data model finished

--- VideoCache/VideoCache.py
+++ VideoCache/VideoCache.py
@@ -20,6 +20,8 @@ class VideoCache:
         self.file = open(self.filename)
         self.num_of_videos, self.num_of_endpoints, self.num_of_request_descriptions, self.num_of_cache_servers, self.capacity_of_cache_server = self.get_next_line_list()
 
+        CacheServer.capacity = self.capacity_of_cache_server
+
         data_model.cache_servers = []
         
         for i in range(self.num_of_cache_servers):
@@ -44,9 +46,15 @@ class VideoCache:
                 endpoint.cache_server_connections.append(cache_server_connection)
             data_model.endpoints.append(endpoint)
 
+        data_model.request_descriptions = []
 
+        for i in range(self.num_of_request_descriptions):
+            video_ID, endpoint_ID, num_of_requests = self.get_next_line_list()
+            video = data_model.videos[video_ID]
+            endpoint = data_model.endpoints[endpoint_ID]
+            data_model.request_descriptions.append(RequestDescrption(video, endpoint, num_of_requests))
 
-        print ""
+        return data_model
 
 class DataModel:
     pass
@@ -72,6 +80,13 @@ class CacheServerConnection:
         self.endpoint = endpoint
         self.latency = cache_server_latency
 
+class RequestDescrption:
+    def __init__(self, video, endpoint, num_of_requests):
+        self.video = video
+        self.endpoint = endpoint
+        self.num_of_requests = num_of_requests
+
 if __name__ == "__main__":
     proc = VideoCache(os.path.join("input", "me_at_the_zoo.in"))
     data_model = proc.get_data_model()
+    print("")
